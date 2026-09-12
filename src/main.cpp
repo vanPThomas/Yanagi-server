9396476d44d4841f6573ace1c4af9e989add0543
thomas <thomas.van.petegem@hotmail.com>
2026-09-13 00:07:48 +0200

    added cout to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
+    std::cout << "Yanagi booting...\n";
     int PORT = 9001;
     int maxClients = 5;
+
+    std::cout << "Listening for connections on port" << PORT.cstr() << ".\n";
 
     Server server(PORT, maxClients);
 
